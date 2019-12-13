91bbd3549e6f6117f5213344225713147eace416
Justin Branch <justinms@outlook.com>
2019-12-13 20:37:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    last_element = array.pop
+    array.join(", ") << ", and #{last_element}"
+  elsif array.length == 2 
+    array.join(" and ")
+  else 
+    array.join 
+  end 
+  
 end

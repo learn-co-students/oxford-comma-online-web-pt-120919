d45945450c7ec7bf860ca5ba852046e44f0ac18c
Jade Lynn Masker <hopefulfire@gmail.com>
2019-12-13 16:02:08 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  last_word = array.pop
+  if array.length != 0
+    array_with_commas = array.join(', ')
+    if array.length == 1
+      oxford_phrase = "#{array_with_commas} and #{last_word}"
+    else
+      oxford_phrase = "#{array_with_commas}, and #{last_word}"
+    end
+  else
+    oxford_phrase = last_word
+  end
+  oxford_phrase
 end

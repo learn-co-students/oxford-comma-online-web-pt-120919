ac304966383bca7b9fc31b02e3035ab1ca16decc
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-21 01:20:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  
+  if array.size == 1
+    return array.join
+    
+    elsif array.size == 2
+    return array.join( " and " )
+
+else 
+  
+    array[-1].prepend "and "
+	array.join(", ")
+end
 
 end

2349eb57014a1facb5a3ad2399d1068a7aa6b0b4
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-21 14:16:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+oxford_comma(array)
+# Array doesn't need to be provided externally, is already defined
+# Split into a string using join method
+# Push "and" to be the second to last element in the array
 
+
+array = ["apple"]
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
+end
+
+oxford_comma(array)
 end
